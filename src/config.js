485dbbfcffa8049adfc1e74fcbead526b95cdca5
nicfer21 <ferboncun@gmail.com>
2023-11-25 14:59:25 -0500

    Cambio FL0

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || "1234";
 export const DB_PORT = process.env.DB_PORT || "5432"; */
 
 //Google cloud
-export const DB_HOST = process.env.DB_HOST || "34.122.139.215";
+export const DB_HOST = process.env.DB_HOST || "ep-delicate-term-56466300.us-east-2.aws.neon.fl0.io";
 export const DB_NAME = process.env.DB_NAME || "mantenimiento";
-export const DB_USER = process.env.DB_USER || "postgres";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "12mantenimiento34";
+export const DB_USER = process.env.DB_USER || "fl0user";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "mt1rwbXu6VMT";
 export const DB_PORT = process.env.DB_PORT || "5432";
