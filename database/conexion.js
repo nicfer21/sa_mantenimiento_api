3f99523961b269afd262446f8f49a6dac41b7049
nicfer21 <ferboncun@gmail.com>
2023-11-25 15:40:23 -0500

    Arreglar FL0

--- database/conexion.js
+++ database/conexion.js
@@ -7,13 +7,22 @@ import {
   DB_USER,
 } from "../src/config.js";
 
-const db = new Sequelize({
+const db = new Sequelize(
+  "postgres://fl0user:mt1rwbXu6VMT@ep-delicate-term-56466300.us-east-2.aws.neon.fl0.io:5432/mantenimiento?sslmode=require"
+);
+
+/* {
   host: DB_HOST,
   database: DB_NAME,
   dialect: "postgres",
   username: DB_USER,
   password: DB_PASSWORD,
   port: DB_PORT,
-});
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync("./cert.pem"),
+    },
+  },
+} */
 
 export default db;
