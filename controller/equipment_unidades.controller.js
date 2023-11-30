f27c2f5d4178b5542d7b76b9849caae4c16306a4
nicfer21 <ferboncun@gmail.com>
2023-11-29 20:37:21 -0500

    Imagenes de Equipos

--- controller/equipment_unidades.controller.js
+++ controller/equipment_unidades.controller.js
@@ -1,9 +1,43 @@
 import Unidades from "../model/equipment_unidades.model.js";
+import db from "../database/conexion.js";
+import { QueryTypes } from "sequelize";
 
 export const getAll = async (req, res) => {
   try {
-    const rs = await Unidades.findAll();
+    const rs = await Unidades.findAll({
+      order: [["id_unidades", "ASC"]],
+    });
     res.json(rs);
+  } catch (error) {
+    res.json({
+      error: error,
+    });
+  }
+};
+
+export const getOneCode = async (req, res) => {
+  try {
+    const rs = await db.query(
+      "SELECT * from public.view_sis_unid WHERE codigo_c = ?;",
+      {
+        type: QueryTypes.SELECT,
+        replacements: [req.params.id],
+      }
+    );
+    res.json(rs[0]);
+  } catch (error) {
+    res.json({
+      error: error,
+    });
+  }
+};
+
+export const getView_Sis_Unid_Ubic = async (req, res) => {
+  try {
+    const rs = await db.query(
+      'SELECT * from "public".view_sis_unid_ubic order by id_unidades;'
+    );
+    res.json(rs[0]);
   } catch (error) {
     res.json({
       error: error,
--- routes/equipment_unidades.routes.js
+++ routes/equipment_unidades.routes.js
@@ -1,9 +1,16 @@
 import express from "express";
 
-import { getAll } from "../controller/equipment_unidades.controller.js";
+import {
+  getAll,
+  getOneCode,
+  getView_Sis_Unid_Ubic,
+} from "../controller/equipment_unidades.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAll);
+router.get("/list/", getView_Sis_Unid_Ubic);
+
+router.get("/code/:id", getOneCode);
 
 export default router;
